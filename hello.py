f82213d2cd353b2b2977217535fd36871c03509c
Eugenia Portillo <47845050+its-A@users.noreply.github.com>
2026-04-09 15:24:22 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,48 @@
+Last login: Wed Dec 10 15:59:41 on ttys000
+eugeniaportillo@Eugenias-Work-Laptop ~ % python3
+Python 3.9.6 (default, Aug  8 2025, 19:06:38) 
+[Clang 17.0.0 (clang-1700.3.19.1)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 
+
+
+>>> #this program says hello and asks for your name
+>>> #print('Hello, World!')
+Hello, World!
+>>> #print('What is your name?') #ask for their name
+What is your name?
+>>> #my_name = input('>')
+>#print('It is good to meet you, ' + my_name)
+>>># print('The length of your name is:')
+The length of your name is:
+>>> #print(len(my_name))
+43
+>>> #print('What is your age?') #ask for their age
+What is your age?
+>>># my_age = input('>')
+>#print('You will be ' + str(int(my_age) + 1)+' in a year')
+
+
+while True:
+    print('Please type your name.')
+    name = input('>')
+    if name == 'your name':
+        break
+print('Thank you!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
